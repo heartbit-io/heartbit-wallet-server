91aae129bc67b354ef983b86558c8f626e31c2d2
Vihiga Tyonum <withtvpeter@gmail.com>
2023-04-04 02:56:38 +0100

    feat(forum): add database connection

---
+++ src/util/dbconnection.ts
@@ -0,0 +1,23 @@
+import { log } from 'console';
+import { Dialect, Sequelize } from 'sequelize';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const db_name = process.env.DB_NAME as string;
+const db_user = process.env.DB_USER as string;
+const db_host = process.env.DB_HOST;
+const db_driver = process.env.DB_DRIVER as Dialect;
+const db_password = process.env.DB_PASSWORD;
+
+if (!db_name || !db_user || !db_host || !db_driver || !db_password) {
+    log('Please ensure that you set the database connection parameters');
+    process.exit(1);
+}
+
+const dbconnection = new Sequelize(db_name, db_user, db_password, {
+  host: db_host,
+  dialect: db_driver
+});
+
+export default dbconnection;
