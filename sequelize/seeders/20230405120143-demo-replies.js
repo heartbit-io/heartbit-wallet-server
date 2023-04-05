9133da39001311d098e29d2223330294a207789c
Vihiga Tyonum <withtvpeter@gmail.com>
2023-04-05 18:03:13 +0100

    feat(replies): add database seeding for replies

--- sequelize/seeders/20230405120143-demo-replies.js
+++ sequelize/seeders/20230405120143-demo-replies.js
@@ -1,19 +1,27 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
 'use strict';
+const { faker } = require('@faker-js/faker');
+
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    const data = {
+			question_id: faker.helpers.arrayElement([...Array(50).keys()]),
+			user_pubkey: faker.finance.bitcoinAddress(),
+			content: faker.lorem.paragraph(),
+			best_reply: faker.helpers.arrayElement([true, false]),
+			createdAt: new Date(),
+			updatedAt: new Date(),
+		};
+
+		const dataArray = [];
+		for (let i = 0; i < 50; i++) {
+			dataArray.push(data);
+		}
+
+		return queryInterface.bulkInsert('replies', dataArray);
   },
 
   async down (queryInterface, Sequelize) {
