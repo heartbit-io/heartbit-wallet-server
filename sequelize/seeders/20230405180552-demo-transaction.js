4acf80594ce0e599149ac49ff3bcd37530f9a445
Vihiga Tyonum <withtvpeter@gmail.com>
2023-04-06 12:12:34 +0100

    fix transactions seeder

--- sequelize/seeders/20230405180552-demo-transaction.js
+++ sequelize/seeders/20230405180552-demo-transaction.js
@@ -6,18 +6,20 @@ const {faker} = require('@faker-js/faker');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-	async up(queryInterface, Sequelize) {
-		const data = {
-			from_user_pubkey: faker.finance.bitcoinAddress(),
+  createTransaction() {
+    return {
+      from_user_pubkey: faker.finance.bitcoinAddress(),
 			to_user_pubkey: faker.helpers.arrayElement(['user', 'doctor', 'admin']),
 			amount: faker.finance.amount(),
 			createdAt: new Date(),
 			updatedAt: new Date(),
-		};
+    }
+  },
+	async up(queryInterface, Sequelize) {
 
 		const dataArray = [];
 		for (let i = 0; i < 50; i++) {
-			dataArray.push(data);
+			dataArray.push(this.createTransaction());
 		}
 
 		return queryInterface.bulkInsert('transactions', dataArray);
