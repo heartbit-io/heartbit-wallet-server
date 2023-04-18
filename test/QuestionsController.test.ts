01a0c22df308ce83c422c366d463d890286f78b6
Vihiga Tyonum <withtvpeter@gmail.com>
2023-04-18 15:44:51 +0100

    refactor tests

--- test/QuestionsController.test.ts
+++ test/QuestionsController.test.ts
@@ -3,8 +3,12 @@ import {expect} from 'chai';
 import {agent as request} from 'supertest';
 import {HttpCodes} from '../src/util/HttpCodes';
 import {faker} from '@faker-js/faker';
-import {QuestionInstance, QuestionStatus} from '../src/models/QuestionModel';
-import {UserInstance} from '../src/models/UserModel';
+import {
+	QuestionAttributes,
+	QuestionInstance,
+	QuestionStatus,
+} from '../src/models/QuestionModel';
+import {UserAttributes, UserInstance} from '../src/models/UserModel';
 
 const base_url = '/api/v1';
 
@@ -21,6 +25,31 @@ describe('Questions endpoints', () => {
 			btc_balance: Number(faker.finance.amount()),
 		};
 	};
+	const createUser = async (user: UserAttributes) => {
+		const result = await request(app)
+			.post(base_url + '/users')
+			.send({...user})
+			.set('Accept', 'application/json');
+		return result;
+	};
+
+	const failureResponse = (status: boolean, statusCode: HttpCodes) => {
+		return {
+			success: status,
+			statusCode: statusCode,
+		};
+	};
+
+	const successResponse = (
+		status: boolean,
+		statusCode: HttpCodes,
+		message: string,
+	) => {
+		return {
+			...failureResponse(status, statusCode),
+			message,
+		};
+	};
 
 	const newQuestion = () => {
 		return {
@@ -30,111 +59,90 @@ describe('Questions endpoints', () => {
 				faker.finance.bitcoinAddress() + new Date().getTime().toString(),
 		};
 	};
+	const createQuestion = async (question: QuestionAttributes) => {
+		return await request(app)
+			.post(base_url + '/questions')
+			.send({
+				...question,
+			})
+			.set('Accept', 'application/json');
+	};
 
 	describe('create question', () => {
 		it('should create a question', async () => {
 			const user = newUser();
-			await request(app)
-				.post(base_url + '/users')
-				.send({...user})
-				.set('Accept', 'application/json');
+			await createUser(user);
 
 			const question = newQuestion();
-			const user_pubkey = user.pubkey;
-			const bounty_amount = user.btc_balance / 2;
-			const response = await request(app)
-				.post(base_url + '/questions')
-				.send({
-					...question,
-					user_pubkey,
-					bounty_amount,
-				})
-				.set('Accept', 'application/json');
+			const question_request = {
+				...question,
+				user_pubkey: user.pubkey,
+				bounty_amount: user.btc_balance / 2,
+			};
+
+			const response = await createQuestion(question_request);
 
 			expect(response.status).to.equal(HttpCodes.CREATED);
-			expect(response.body).to.include({
-				success: true,
-				statusCode: HttpCodes.CREATED,
-				message: 'Question posted successfully',
-			});
-			expect(response.body.data).to.contain({
-				...question,
-				user_pubkey,
-				bounty_amount,
-			});
+			expect(response.body).to.include(
+				successResponse(
+					true,
+					HttpCodes.CREATED,
+					'Question posted successfully',
+				),
+			);
+			expect(response.body.data).to.contain(question_request);
 		});
 
 		it('should return validator error if user public key is not registered', async () => {
 			const question = newQuestion();
-			const response = await request(app)
-				.post(base_url + '/users')
-				.send({...question})
-				.set('Accept', 'application/json');
+			const response = await createQuestion(question);
 			expect(response.status).to.equal(HttpCodes.BAD_REQUEST);
-			expect(response.body).to.include({
-				success: false,
-				statusCode: HttpCodes.BAD_REQUEST,
-			});
+			expect(response.body).to.include(
+				failureResponse(false, HttpCodes.BAD_REQUEST),
+			);
 		});
 
-		it('should return validator error role if bounty amount is higher than user btc balance', async () => {
+		it('should return validator error if bounty amount is higher than user btc balance', async () => {
 			const user = newUser();
-			await request(app)
-				.post(base_url + '/users')
-				.send({...user})
-				.set('Accept', 'application/json');
+			await createUser(user);
 
 			const question = newQuestion();
-			const user_pubkey = user.pubkey;
-			const bounty_amount = user.btc_balance + 2;
-			const response = await request(app)
-				.post(base_url + '/questions')
-				.send({
-					...question,
-					user_pubkey,
-					bounty_amount,
-				})
-				.set('Accept', 'application/json');
-
+			const question_request = {
+				...question,
+				user_pubkey: user.pubkey,
+				bounty_amount: user.btc_balance + 2,
+			};
+			const response = await createQuestion(question_request);
 			expect(response.status).to.equal(HttpCodes.BAD_REQUEST);
-			expect(response.body).to.include({
-				success: false,
-				statusCode: HttpCodes.BAD_REQUEST,
-			});
+			expect(response.body).to.include(
+				failureResponse(false, HttpCodes.BAD_REQUEST),
+			);
 		});
 
 		it('should return validator error role if user public key is not supplied', async () => {
 			const user = newUser();
-			await request(app)
-				.post(base_url + '/users')
-				.send({...user})
-				.set('Accept', 'application/json');
+			await createUser(user);
 
 			const question = newQuestion();
-			const user_pubkey = null;
-			const bounty_amount = user.btc_balance / 2;
+			const question_request = {
+				...question,
+				user_pubkey: null,
+				bounty_amount: user.btc_balance / 2,
+			};
 			const response = await request(app)
 				.post(base_url + '/questions')
-				.send({
-					...question,
-					user_pubkey,
-					bounty_amount,
-				})
+				.send(question_request)
 				.set('Accept', 'application/json');
 
 			expect(response.status).to.equal(HttpCodes.BAD_REQUEST);
-			expect(response.body).to.include({
-				success: false,
-				statusCode: HttpCodes.BAD_REQUEST,
-			});
+			expect(response.body).to.include(
+				failureResponse(false, HttpCodes.BAD_REQUEST),
+			);
 		});
 
 		it('should return validator error role if question content is not supplied', async () => {
 			const user = newUser();
-			await request(app)
-				.post(base_url + '/users')
-				.send({...user})
-				.set('Accept', 'application/json');
+			await createUser(user);
 
 			const question = newQuestion();
 			const user_pubkey = user.pubkey;
@@ -151,18 +159,14 @@ describe('Questions endpoints', () => {
 				.set('Accept', 'application/json');
 
 			expect(response.status).to.equal(HttpCodes.BAD_REQUEST);
-			expect(response.body).to.include({
-				success: false,
-				statusCode: HttpCodes.BAD_REQUEST,
-			});
+			expect(response.body).to.include(
+				failureResponse(false, HttpCodes.BAD_REQUEST),
+			);
 		});
 
 		it('should return validator error role if bounty amount is not supplied', async () => {
 			const user = newUser();
-			await request(app)
-				.post(base_url + '/users')
-				.send({...user})
-				.set('Accept', 'application/json');
+			await createUser(user);
 
 			const question = newQuestion();
 			const user_pubkey = user.pubkey;
@@ -177,147 +181,115 @@ describe('Questions endpoints', () => {
 				.set('Accept', 'application/json');
 
 			expect(response.status).to.equal(HttpCodes.BAD_REQUEST);
-			expect(response.body).to.include({
-				success: false,
-				statusCode: HttpCodes.BAD_REQUEST,
-			});
+			expect(response.body).to.include(
+				failureResponse(false, HttpCodes.BAD_REQUEST),
+			);
 		});
 	});
 	describe('get all questions', () => {
 		it('should return all questions', async () => {
 			const user = newUser();
-			await request(app)
-				.post(base_url + '/users')
-				.send({...user})
-				.set('Accept', 'application/json');
+			await createUser(user);
 			const question = newQuestion();
-			const user_pubkey = user.pubkey;
-			const bounty_amount = user.btc_balance / 2;
-			await request(app)
-				.post(base_url + '/questions')
-				.send({
-					...question,
-					user_pubkey,
-					bounty_amount,
-				})
-				.set('Accept', 'application/json');
+			const question_request = {
+				...question,
+				user_pubkey: user.pubkey,
+				bounty_amount: user.btc_balance / 2,
+			};
+			await createQuestion(question_request);
 
 			const response = await request(app).get(`${base_url}/questions`);
 			expect(response.status).to.equal(HttpCodes.OK);
-			expect(response.body).to.include({
-				success: true,
-				statusCode: HttpCodes.OK,
-				message: 'Successfully retrieved all questions',
-			});
+			expect(response.body).to.include(
+				successResponse(
+					true,
+					HttpCodes.OK,
+					'Successfully retrieved all questions',
+				),
+			);
 			expect(response.body.data).to.be.an('array');
 		});
 
 		it('should return all open questions', async () => {
 			const user = newUser();
-			await request(app)
-				.post(base_url + '/users')
-				.send({...user})
-				.set('Accept', 'application/json');
+			await createUser(user);
 			const question = newQuestion();
-			const user_pubkey = user.pubkey;
-			const status = QuestionStatus.Open;
-			const bounty_amount = user.btc_balance / 2;
-			await request(app)
-				.post(base_url + '/questions')
-				.send({
-					...question,
-					user_pubkey,
-					status,
-					bounty_amount,
-				})
-				.set('Accept', 'application/json');
+			const question_body = {
+				...question,
+				user_pubkey: user.pubkey,
+				status: QuestionStatus.Open,
+				bounty_amount: user.btc_balance / 2,
+			};
+
+			await createQuestion(question_body);
 			const response = await request(app).get(`${base_url}/questions/open`);
 			expect(response.status).to.equal(HttpCodes.OK);
-			expect(response.body).to.include({
-				success: true,
-				statusCode: HttpCodes.OK,
-				message: 'Successfully retrieved all open questions',
-			});
+			expect(response.body).to.include(
+				successResponse(
+					true,
+					HttpCodes.OK,
+					'Successfully retrieved all open questions',
+				),
+			);
 			expect(response.body.data).to.be.an('array');
-			expect(response.body.data[0].status).to.equal(QuestionStatus.Open);
+			expect(response.body.data[0].status).to.equal(question_body.status);
 		});
 	});
 	describe('it should get a question by id', () => {
 		it('should return a question by id', async () => {
 			const user = newUser();
-			await request(app)
-				.post(base_url + '/users')
-				.send({...user})
-				.set('Accept', 'application/json');
+			await createUser(user);
 			const question = newQuestion();
-			const user_pubkey = user.pubkey;
-			const status = QuestionStatus.Open;
-			const bounty_amount = user.btc_balance / 2;
-			const create_question = await request(app)
-				.post(base_url + '/questions')
-				.send({
-					...question,
-					user_pubkey,
-					status,
-					bounty_amount,
-				})
-				.set('Accept', 'application/json');
+			const question_body = {
+				...question,
+				user_pubkey: user.pubkey,
+				status: QuestionStatus.Open,
+				bounty_amount: user.btc_balance / 2,
+			};
+			const create_question = await createQuestion(question_body);
 
 			const response = await request(app).get(
 				`${base_url}/questions/${create_question.body.data.id}`,
 			);
 			expect(response.status).to.equal(HttpCodes.OK);
-			expect(response.body).to.include({
-				success: true,
-				statusCode: HttpCodes.OK,
-				message: 'Successfully retrieved question details',
-			});
-			expect(response.body.data).to.include({
-				...question,
-				user_pubkey,
-				status,
-				bounty_amount,
-			});
+			expect(response.body).to.include(
+				successResponse(
+					true,
+					HttpCodes.OK,
+					'Successfully retrieved question details',
+				),
+			);
+			expect(response.body.data).to.include(question_body);
 			expect(response.body.data)
 				.to.have.property('replies')
 				.that.is.an('array');
-        });
-        
-    });
-    
-    describe('it should delete a question by id', () => { 
-        it('should delete a question by id', async () => {
-            const user = newUser();
-            await request(app)
-                .post(base_url + '/users')
-                .send({...user})
-                .set('Accept', 'application/json');
-            const question = newQuestion();
-            const user_pubkey = user.pubkey;
-            const status = QuestionStatus.Open;
-            const bounty_amount = user.btc_balance / 2;
-            const create_question = await request(app)
-                .post(base_url + '/questions')
-                .send({
-                    ...question,
-                    user_pubkey,
-                    status,
-                    bounty_amount,
-                })
-                .set('Accept', 'application/json');
-    
-            const response = await request(app).delete(
-                `${base_url}/questions/${create_question.body.data.id}`,
-            ).send({
-                user_pubkey
-            }).set('Accept', 'application/json');
-            expect(response.status).to.equal(HttpCodes.OK);
-            expect(response.body).to.include({
-                success: true,
-                statusCode: HttpCodes.OK,
-                message: 'Question deleted successfully',
-            });
-        });
-    });
+		});
+	});
 
+	describe('it should delete a question by id', () => {
+		it('should delete a question by id', async () => {
+			const user = newUser();
+			await createUser(user);
+			const question = newQuestion();
+			const question_body = {
+				...question,
+				user_pubkey: user.pubkey,
+				status: QuestionStatus.Open,
+				bounty_amount: user.btc_balance / 2,
+			};
+
+			const create_question = await createQuestion(question_body);
+
+			const response = await request(app)
+				.delete(`${base_url}/questions/${create_question.body.data.id}`)
+				.send({
+					user_pubkey: question_body.user_pubkey,
+				})
+				.set('Accept', 'application/json');
+			expect(response.status).to.equal(HttpCodes.OK);
+			expect(response.body).to.include(
+				successResponse(true, HttpCodes.OK, 'Question deleted successfully'),
+			);
+		});
+	});
 });
