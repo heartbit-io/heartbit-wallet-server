7c35cbb98f7c11abc6059d791d72c1e186045041
david <david@heartbit.io>
2023-04-27 22:26:46 +0900

    feat(reply): add get chatgpt and get reply api

--- src/controllers/RepliesController.ts
+++ src/controllers/RepliesController.ts
@@ -1,12 +1,133 @@
 import {Request, Response} from 'express';
-import {HttpCodes} from '../util/HttpCodes';
+
+import ChatgptService from '../services/ChatgptService';
 import FormatResponse from '../lib/FormatResponse';
-import ReplyService from '../services/ReplyService';
+import {HttpCodes} from '../util/HttpCodes';
 import QuestionService from '../services/QuestionService';
-import UserService from '../services/UserService';
+import ReplyService from '../services/ReplyService';
+import {ReplyTypes} from '../util/enums/replyTypes';
 import TransactionService from '../services/TransactionService';
+import UserService from '../services/UserService';
 
 class RepliesController {
+	async getChatgptReply(
+		req: Request,
+		res: Response,
+	): Promise<Response<FormatResponse>> {
+		try {
+			const {questionId} = req.params;
+			const questionContent = req.query.questionContent as string;
+
+			const model = 'gpt-3.5-turbo';
+			const maxTokens = 2048;
+			const chatgptReply = await ChatgptService.create(
+				Number(questionId),
+				questionContent,
+				model,
+				maxTokens,
+			);
+
+			if (!chatgptReply) {
+				return res
+					.status(HttpCodes.NOT_FOUND)
+					.json(
+						new FormatResponse(
+							false,
+							HttpCodes.NOT_FOUND,
+							'Chatgpt reply was not found',
+							null,
+						),
+					);
+			}
+
+			const reply = chatgptReply.jsonAnswer.triageGuide;
+			const createdAt = chatgptReply.createdAt;
+
+			return res.status(HttpCodes.OK).json(
+				new FormatResponse(true, HttpCodes.OK, 'Chatgpt reply successfully', {
+					reply,
+					createdAt,
+				}),
+			);
+		} catch (error) {
+			return res
+				.status(HttpCodes.INTERNAL_SERVER_ERROR)
+				.json(
+					new FormatResponse(
+						false,
+						HttpCodes.INTERNAL_SERVER_ERROR,
+						error,
+						null,
+					),
+				);
+		}
+	}
+
+	async get(req: Request, res: Response): Promise<Response<FormatResponse>> {
+		try {
+			const {questionId} = req.params;
+			let replyType: ReplyTypes;
+			let reply: string;
+			let name: string;
+			let classification: string;
+			let updatedAt: Date;
+			const replyForDoctor = await ReplyService.getReplyByQuestionId(
+				Number(questionId),
+			);
+
+			if (replyForDoctor) {
+				replyType = ReplyTypes.DOCTOR;
+				name = replyForDoctor.user_email;
+				reply = replyForDoctor.content; // TODO(david) Add Health records using JSON format
+				classification = 'General physician'; // TODO(david) Get from user
+				updatedAt = replyForDoctor.updatedAt;
+			} else {
+				const replyForChatGpt =
+					await ChatgptService.getChatGptReplyByQuestionId(Number(questionId));
+
+				if (!replyForChatGpt) {
+					return res
+						.status(HttpCodes.NOT_FOUND)
+						.json(
+							new FormatResponse(
+								false,
+								HttpCodes.NOT_FOUND,
+								'Chatgpt reply was not found',
+								null,
+							),
+						);
+				}
+				replyType = ReplyTypes.AI;
+				name = 'Trage by GPT-3.5';
+				reply = replyForChatGpt.jsonAnswer.triageGuide;
+				classification = 'Open AI';
+				updatedAt = replyForChatGpt.updatedAt;
+			}
+
+			return res
+				.status(HttpCodes.OK)
+				.json(
+					new FormatResponse(
+						true,
+						HttpCodes.OK,
+						'Reply retrieved successfully',
+						{replyType, name, classification, reply, updatedAt},
+					),
+				);
+		} catch (error) {
+			return res
+				.status(HttpCodes.INTERNAL_SERVER_ERROR)
+				.json(
+					new FormatResponse(
+						false,
+						HttpCodes.INTERNAL_SERVER_ERROR,
+						error,
+						null,
+					),
+				);
+		}
+	}
+
 	async create(req: Request, res: Response): Promise<Response<FormatResponse>> {
 		try {
 			const question = await ReplyService.createReply({...req.body});
--- src/routes/replyRoutes.ts
+++ src/routes/replyRoutes.ts
@@ -1,9 +1,17 @@
-import {Router} from 'express';
 import RepliesController from '../controllers/RepliesController';
 import RepliesValidator from '../lib/RepliesValidator';
+import {Router} from 'express';
 import Validation from '../middleware/Validation';
 
 const router = Router();
+
+router.get('/:questionId', Validation.validate, RepliesController.get);
+
+router.get(
+	'/:questionId/chatGptReply',
+	Validation.validate,
+	RepliesController.getChatgptReply,
+);
 
 router.post(
 	'/',
