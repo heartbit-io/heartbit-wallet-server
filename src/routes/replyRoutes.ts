976e8f9bf0ee27098b583893672d37defccebcc7
ChrisCho-H <hyeonheum@naver.com>
2023-05-03 12:28:49 +0900

    refactor: move createDoctorReply to replyRoutes

--- src/routes/replyRoutes.ts
+++ src/routes/replyRoutes.ts
@@ -1,9 +1,17 @@
 import RepliesController from '../controllers/RepliesController';
 import RepliesValidator from '../lib/RepliesValidator';
+import DoctorsController from '../controllers/DoctorsController';
 import {Router} from 'express';
 import Validation from '../middleware/Validation';
 
 const router = Router();
+
+router.post(
+	'/',
+	RepliesValidator.checkCreateReply(),
+	Validation.validate,
+	DoctorsController.createDoctorReply,
+);
 
 router.delete(
 	'/:replyId',
