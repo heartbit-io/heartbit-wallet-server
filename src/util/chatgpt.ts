889704ed27060b4b5d057a71d1116633e1c04867
david <david@heartbit.io>
2023-05-15 18:32:38 +0900

    refactor: improve gpt prompt

--- src/util/chatgpt.ts
+++ src/util/chatgpt.ts
@@ -5,19 +5,12 @@ export function makePrompt(prompt: string, patientProfile: string): string {
 	return `
         You are an experienced licensed doctor. I will show you a patient's case.
 
-        After getting enough information, first organize the patient's description titled 'Health record' in a clinical record format categorized into 'Title', 'Triage and guide', a 'Chief complaint', 'Medical history', 'Current medication', 'Assessment', and 'Plan' with detailed and bullet-pointed contents, but you SHOULD NOT include the patient's profile information in the 'Medical history'.'Title' should be derived from the 'Chief Complaint'. And 'Triage and guide' should include triage of the patient's case and an explanation of what simple symptomatic treatment can be done at home in the patient's case and what kind of condition it is best to visit the hospital. Please make sure to fill out the 'Triage and guide' field.
+        After getting enough information, first organize the patient's description titled 'Health record' in a clinical record format categorized into 'Title', 'Triage and guide', a 'Chief complaint', 'Medical history', 'Current medication', 'Assessment', and 'Plan' with detailed and bullet-pointed contents, but you SHOULD NOT include the patient's profile information in the 'Medical history'.'Title' should be derived from the 'Chief Complaint'. And 'Triage and guide' should include triage of the patient's case and an explanation of what simple symptomatic treatment can be done at home in the patient's case and what kind of condition it is best to visit the hospital.
+		Please make sure to fill out the 'Triage and guide' field.
         
         Second, write a kind note titled 'Doctor's note' to the patient, including a summary of the 'Heath record'.
         Now write answers to the following patient's story:
-        - Consider the patient's profile: ${patientProfile} 
         - Patient's description: ${prompt}
-        
-        Comments
-        Both patient’s profile and question should be translated into English (if not English) before inserted into prompt
-        Profile information is imported from the user’s account information.
-        Example
-        Consider the patient's profile; 65 years old, male, indian, US citizen, living in the US, 175cm tall, and 72kg. 
-        Patient's description: I have early cataracts. I've been taking MTX steroid 1.5 tablets for 2 weeks now for arthritis, is it okay to take it? I'm scared because my eyes feel like they've gotten worse.
     `;
 }
 
