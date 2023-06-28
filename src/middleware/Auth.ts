220c0c00ef9d8fbc7a68219ac8fa7a62c464b14e
david <david@heartbit.io>
2023-06-28 11:31:57 +0900

    feat: add api key verify logic

--- src/middleware/Auth.ts
+++ src/middleware/Auth.ts
@@ -1,15 +1,33 @@
 import * as Sentry from '@sentry/node';
 
 import {NextFunction, Request, Response} from 'express';
+
 import {HttpCodes} from '../util/HttpCodes';
-import admin from '../config/firebase-config';
 import ResponseDto from '../dto/ResponseDTO';
+import admin from '../config/firebase-config';
 
 export interface DecodedRequest extends Request {
 	email?: string;
 }
 
 class Auth {
+	verifyApiKey(req: Request, res: Response, next: NextFunction) {
+		const isVerified = req?.headers?.apiKey === process.env.API_KEY;
+		if (isVerified) return next();
+
+		Sentry.captureMessage(`[${HttpCodes.UNAUTHORIZED}] Unauthorized - API Key`);
+		return res
+			.status(HttpCodes.UNAUTHORIZED)
+			.json(
+				new ResponseDto(
+					false,
+					HttpCodes.UNAUTHORIZED,
+					'Unauthorized - API Key',
+					null,
+				),
+			);
+	}
+
 	async verifyToken(req: DecodedRequest, res: Response, next: NextFunction) {
 		const token = req?.headers?.authorization?.split(' ')[1] || '';
 
@@ -21,11 +39,18 @@ class Auth {
 				return next();
 			}
 
-			Sentry.captureMessage(`[${HttpCodes.UNAUTHORIZED}] Unauthorized`);
+			Sentry.captureMessage(
+				`[${HttpCodes.UNAUTHORIZED}] Unauthorized - Firebase Token`,
+			);
 			return res
 				.status(HttpCodes.UNAUTHORIZED)
 				.json(
-					new ResponseDto(false, HttpCodes.UNAUTHORIZED, 'Unauthorized', null),
+					new ResponseDto(
+						false,
+						HttpCodes.UNAUTHORIZED,
+						'Unauthorized - Firebase Token',
+						null,
+					),
 				);
 		} catch (error) {
 			Sentry.captureMessage(`[${HttpCodes.INTERNAL_SERVER_ERROR}] ${error}`);
