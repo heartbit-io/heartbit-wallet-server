867a7e47e7f724d4534f6a92eeba306961e3f44e
Vihiga Tyonum <withtvpeter@gmail.com>
2023-08-22 21:47:09 +0100

    add chatgpt retry in the event listener

---
+++ src/events/ChatGptEvent.ts
@@ -0,0 +1,9 @@
+import {EventEmitter} from 'node:events';
+
+class ChatGptEvent extends EventEmitter {
+	constructor() {
+		super();
+	}
+}
+
+export default ChatGptEvent;
---
+++ src/listeners/ChatGptReplyListener.ts
@@ -0,0 +1,59 @@
+import ChatGptRepository from '../Repositories/ChatGptRepository';
+import QuestionRepository from '../Repositories/QuestionRepository';
+import * as Sentry from '@sentry/node';
+import DeeplService from '../services/DeeplService';
+import {QuestionAttributes} from '../domains/entities/Question';
+import ChatGptEvent from '../events/ChatGptEvent';
+import ChatgptService from '../services/ChatgptService';
+import {QuestionTypes} from '../util/enums';
+
+const ChatGptReplyListener = new ChatGptEvent();
+
+const onChatGptReplyEvent = async (questionId: number) => {
+	const model = 'gpt-3.5-turbo';
+	const maxTokens = 2048;
+	try {
+		const question = await QuestionRepository.getQuestion(questionId);
+
+		if (!question) {
+			Sentry.captureMessage(`error getting question for chatgpt response`);
+			return;
+		}
+		const rawContentLanguage: any = question.rawContentLanguage;
+		const questionAttr = question as QuestionAttributes;
+
+		const chatgptReply = await ChatgptService.create(
+			questionAttr,
+			model,
+			maxTokens,
+		);
+
+		if (!chatgptReply) {
+			Sentry.captureMessage(`error creating chatgpt response`);
+			return;
+		}
+
+		const translateText =
+			questionAttr.type === QuestionTypes.GENERAL
+				? chatgptReply.jsonAnswer.aiAnswer
+				: chatgptReply.jsonAnswer.guide;
+
+		const translatedReply = await DeeplService.getTextTranslatedIntoEnglish(
+			translateText,
+			rawContentLanguage,
+		);
+
+		await ChatGptRepository.updateTranslatedChatGptReply(
+			chatgptReply.id,
+			translatedReply.text,
+		);
+	} catch (error) {
+		Sentry.captureMessage(
+			`error creating chatgpt response in events: ${error}`,
+		);
+	}
+};
+
+ChatGptReplyListener.on('questionIdForChatGptReply', onChatGptReplyEvent);
+
+export default ChatGptReplyListener;
